15dc5e5d6c7d308a388b4ca460a0c2d1d2fb9dac
Sachin Rathore <154586309+realsachinr@users.noreply.github.com>
2024-03-29 23:45:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const button = document.querySelector('.btn-no');
-const btnYes = document.querySelector('.btn-yes');
-const boxContainer = document.querySelector('.box-container');
+const button = document.querySelector(".btn-no");
+const btnYes = document.querySelector(".btn-yes");
+const boxContainer = document.querySelector(".box-container");
+const gif = document.querySelector(".tenor-gif-embed");
 
+button.addEventListener("mouseover", () => {
+  const maxX = window.innerWidth - button.offsetWidth;
+  const maxY = window.innerHeight - button.offsetHeight;
 
-  button.addEventListener('mouseover', () => {
-    button.style.opacity = '0'; // Change this value to the desired position
-  });
+  const newX = Math.floor(Math.random() * maxX);
+  const newY = Math.floor(Math.random() * maxY);
 
-  button.addEventListener('mouseout', () => {
-    button.style.opacity = '1'; // Reset to the original position
-  });
+  button.style.left = `${newX}px`;
+  button.style.top = `${newY}px`;
+});
 
+btnYes.addEventListener("click", () => {
+  gif.style.display = "block";
+  // let newpara = document.createElement('p');
+  // newpara.textContent = "Okay Come Hug ME"
 
-
-  btnYes.addEventListener('click', () => {
-
-    let newpara = document.createElement('p');
-    newpara.textContent = "Okay Come Hug ME"
-
-    boxContainer.appendChild(newpara);
-  });
-
+  // boxContainer.appendChild(newpara);
+});
